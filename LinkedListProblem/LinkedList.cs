214b038331eb4b40069ff5c348581c176177d2b9
Jogan Daniel A <jogan.d@tvsnext.com>
2023-07-26 12:53:34 +0530

    [jogan] Add : Linked list and Stack implemented

---
+++ LinkedListProblem/LinkedList.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace LinkedListProblem
+{
+    public class LinkedList
+    {
+        internal Node head;
+        internal void Add(int data)
+        {
+            Node node = new Node(data);
+            if(this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while(temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into linked list" , node.data);
+        }
+        internal void AddInReverseOrder(int data)
+        {
+            Node newnode = new Node(data);
+            if (this.head == null)
+            {
+                this.head = newnode;
+            }
+            else
+            {
+                Node temp = this.head;
+                head = newnode;
+                head.next = temp;
+            }
+        }
+        internal Node InsertAtParticularPosition(int position,int data)
+        {
+            Node newestNode = new Node(data);   
+            if(this.head == null)
+            {
+                return newestNode;
+            }
+            if(position==0)
+            {
+                newestNode.next = this.head;
+                this.head.next = newestNode;
+                return this.head;
+            }
+            Node prev = null;
+            Node current = this.head;
+            int count = 0;
+            while(current!=null && count < position)
+            {
+                prev = current;
+                current = current.next; 
+                count++;
+            }
+            newestNode.next = prev.next;
+            prev.next = newestNode;
+            return this.head;
+        }
+        internal Node RemoveFirstNode()
+        {
+            if(this.head == null)
+            {
+                return null;
+            }
+            this.head = this.head.next;
+            return this.head;
+        }
+        internal Node RemoveLastNode()
+        {
+            if (this.head == null)
+            {
+                return null;
+            }
+            if(head.next == null)
+            {
+                return null;
+            }
+            Node NewNode = head;
+            while(NewNode.next.next != null)
+            {
+                NewNode = NewNode.next;
+            }
+            NewNode.next = null;
+            return head;
+        }
+        internal int Search(int value)
+        {
+            Node node = this.head;
+            int count = 0;
+            while(node != null)
+            {
+                if(node.data==value)
+                {
+                    return count;
+                }
+               node = node.next;
+                count++;
+            }
+            return count;
+        }
+        internal void DeleteNodeAtParticularPosition(int position)
+        {
+            if (this.head == null)
+            {
+                Console.WriteLine("LinkedList is empty");
+                return;
+            }
+            Node temp = this.head;
+            if(position == 0)
+            {
+                this.head = temp.next;
+                return;
+            }
+            for(int i=0;temp!=null&& i<position-1;i++) { 
+            temp= temp.next;
+            }   
+            if(temp == null || temp.next==null)
+            {
+                return;
+            }
+            Node next = temp.next.next;
+            temp.next = next;
+        }
+        internal void Size()
+        {
+            Node temp = this.head;
+            int count = 0;
+            if (temp == null)
+            {
+                Console.WriteLine("Linked list is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                temp = temp.next;
+                count++;
+            }
+            Console.WriteLine("Length of linked list is "+count);
+        }
+            internal void Display()
+        {
+            Node temp = this.head;
+            if(temp ==  null)
+            {
+                Console.WriteLine("Linked list is empty");
+                return;
+            }
+            while(temp != null)
+            {
+                Console.Write(temp.data+" ");
+                temp = temp.next;
+            }
+        }
+    }
+}
---
+++ LinkedListProblem/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace LinkedListProblem
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to linked List program!");
+            //LinkedList list = new LinkedList();
+            LinkedListStack stack = new LinkedListStack();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Display();
+            stack.Peek();
+            stack.Pop();
+            stack.Display();
+
+            //list.Add(56);
+            //list.Add(30);
+            //list.Add(70);
+            //list.Add(90);
+            ////list.RemoveFirstNode();
+            ////Console.WriteLine(list.Search(70));
+            ////list.Display();
+            //list.DeleteNodeAtParticularPosition(2);
+            //list.Display();
+            //list.Size();
+            //list.RemoveLastNode();
+            //list.Display();
+            //list.AddInReverseOrder(56);
+            //list.AddInReverseOrder(30);
+            //list.AddInReverseOrder(70);
+            //list.InsertAtParticularPosition(2, 40);
+            //list.Display();   
+        }
+    }
+}
